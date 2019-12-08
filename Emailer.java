9e5733cce07362d8735791f1f08800d2105343cb
Nat Panzarini <natalio.panzarini@sjsu.edu>
2019-12-08 15:37:54 -0800

    Emailer and Parser update

---
+++ Emailer.java
@@ -0,0 +1,153 @@
+package test.test1;
+
+import java.util.*; 
+import javax.mail.*; 
+import javax.mail.internet.*; 
+import javax.activation.*; 
+import javax.mail.Session; 
+import javax.mail.Transport;
+
+public class Emailer  
+{
+	private static String sender = "sjsuflighttracker@gmail.com";
+	private static String recipient;
+	private static String origin;
+	private static String destination;
+	private static String date; // needs to be XX-XX-XX format
+	private static String targetPrice;
+	private static String currPrice;
+	
+	private static String body;
+	private static String subject;
+	
+	public Emailer(String recipient) {
+		this.recipient = recipient;
+	}
+	public Emailer(String recipient,String origin,String destination, String targetPrice, String currPrice) {
+		this.recipient = recipient;
+		this.origin = origin;
+		this.destination = destination;
+		//this.date = date;
+		this.targetPrice = targetPrice;
+	}
+	
+	public void sendMail(String subject, String body) {
+		
+		this.subject = subject;
+		this.body = body;
+		
+	      // Setting up mail server 
+	      Properties properties = new Properties();
+	      properties.put("mail.transport.protocol", "smtp");
+	      properties.put("mail.smtp.host", "smtp.gmail.com");
+	      properties.put("mail.smtp.port", "587");
+	      
+	      //Issue STARTTLS command
+	      properties.put("mail.smtp.starttls.enable", "true");
+	  
+	      //Authentication for host email
+	      properties.put("mail.smtp.auth", "true");
+	      
+	      Authenticator authenticator = new Authenticator() {
+	          protected PasswordAuthentication getPasswordAuthentication() {
+	              return new PasswordAuthentication("sjsuflighttracker@gmail.com", "memes1337");
+	          }
+	      };
+	      
+	      // creating session object to get properties 
+	      Session session = Session.getDefaultInstance(properties, authenticator);
+	  
+	      try 
+	      { 
+	         // MimeMessage object. 
+	         MimeMessage message = new MimeMessage(session); 
+	  
+	         // Set From Field: adding senders email to from field. 
+	         message.setFrom(new InternetAddress(sender)); 
+	  
+	         // Set To Field: adding recipient's email to from field.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+	  
+	         // Set Subject: subject of the email 
+	         message.setSubject(subject); 
+	  
+	         // set body of the email. 
+	         message.setText(body);
+	  
+	         // Send email. 
+	         Transport.send(message); 
+	         System.out.println("Mail successfully sent :)"); 
+	      } 
+	      catch (MessagingException mex)  
+	      { 
+	         mex.printStackTrace(); 
+	      }
+	}
+	
+	
+	//@Overload
+	public void sendMail() {
+		body = "Flight " + origin + " to " + destination + " has fallen below " + targetPrice
+				+ "and is now " + currPrice + ". Prices are subject to change quickly, so book now!";
+		
+		subject = "Flight Tracker Update";
+		
+	      // Setting up mail server 
+	      Properties properties = new Properties();
+	      properties.put("mail.transport.protocol", "smtp");
+	      properties.put("mail.smtp.host", "smtp.gmail.com");
+	      properties.put("mail.smtp.port", "587");
+	      
+	      //Issue STARTTLS command
+	      properties.put("mail.smtp.starttls.enable", "true");
+	  
+	      //Authentication for host email
+	      properties.put("mail.smtp.auth", "true");
+	      
+	      Authenticator authenticator = new Authenticator() {
+	          protected PasswordAuthentication getPasswordAuthentication() {
+	              return new PasswordAuthentication("sjsuflighttracker@gmail.com", "memes1337");
+	          }
+	      };
+	      
+	      // creating session object to get properties 
+	      Session session = Session.getDefaultInstance(properties, authenticator);
+	  
+	      try 
+	      { 
+	         // MimeMessage object. 
+	         MimeMessage message = new MimeMessage(session); 
+	  
+	         // Set From Field: adding senders email to from field. 
+	         message.setFrom(new InternetAddress(sender)); 
+	  
+	         // Set To Field: adding recipient's email to from field.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+	  
+	         // Set Subject: subject of the email 
+	         message.setSubject(subject); 
+	  
+	         // set body of the email. 
+	         message.setText(body);
+	  
+	         // Send email. 
+	         Transport.send(message); 
+	         System.out.println("Mail successfully sent :)"); 
+	      } 
+	      catch (MessagingException mex)  
+	      { 
+	         mex.printStackTrace(); 
+	      }
+	}
+  
+   public static void main(String [] args)  
+   {     
+	   Emailer emailer = new Emailer("devine951@gmail.com");
+	   
+	   for(int i = 0; i < 10; i++) {
+		   emailer.sendMail("ur awesome", "i just wanna elaborate that ur awesome!!!");
+	   }
+	   
+   
+   } 
+} 
--- Parser.java
+++ Parser.java
@@ -1,27 +1,39 @@
+package test.test1;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Parser{
-  private static Double minPrice;
-  private static JSONObject obj;
+	  private static Double minPrice;
+	  private static JSONObject obj;
 
-  public Parser(JSONObject obj){
-    this.obj = obj;
-  }
+	  public Parser(JSONObject obj){
+	    this.obj = obj;
+	  }
 
-  public static Double getMin(){
-    Double min = (Double) obj.get("Quotes");
-    return minPrice;
-  }
+	  public static Double getMin(){
+	    JSONArray quotes = (JSONArray) obj.get("Quotes");
+	    JSONObject quotesItems = (JSONObject) quotes.get(0);
+	    minPrice = (Double) quotesItems.get("MinPrice");
+	    
+	    return minPrice;
+	  }
 
-  public static void main(String[] args) {
-    // parsing file "JSONExample.json"
-        Object obj = new JSONParser().parse(new FileReader("JSONExample.json"));
+	  public static void main(String[] args) throws Exception{
+	    // parsing file "JSONExample.json"
+	        Object obj = new JSONParser().parse(new FileReader("flightTracker.json"));
 
-        // typecasting obj to JSONObject
-        JSONObject jo = (JSONObject) obj;
+	        // typecasting obj to JSONObject
+	        JSONObject jo = (JSONObject) obj;
+	        
+	        Parser parser = new Parser(jo);
 
-        System.out.println("Minimum Price among all airlines: " + getMin());
+	        System.out.println("Minimum Price among all airlines: " + getMin());
+	  }
 
 
-  }
+	}
 
-
-}

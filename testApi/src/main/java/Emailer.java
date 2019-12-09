158caf6a6f031325b6f7a3bd164b6919a1673761
jan <rodriguez.janrichard@gmail.com>
2019-12-08 18:10:16 -0800

    Pushed entire maven project onto repo

---
+++ testApi/src/main/java/Emailer.java
@@ -0,0 +1,154 @@
+
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+import javax.mail.Session;
+import javax.mail.Transport;
+
+public class Emailer
+{
+	private static String sender = "sjsuflighttracker@gmail.com";
+	private static String recipient;
+	private static String origin;
+	private static String destination;
+	private static String date; // needs to be YYYY-MM-DD format
+	private static String targetPrice;
+	private static String currPrice;
+
+	private static String body;
+	private static String subject;
+
+	public Emailer(String recipient) {
+		this.recipient = recipient;
+	}
+	/**
+	 *
+	 * @param recipient
+	 * @param origin
+	 * @param destination
+	 * @param targetPrice
+	 * @param currPrice
+	 */
+	public Emailer(String recipient,String origin,String destination,String date, String targetPrice, String currPrice) {
+		this.recipient = recipient;
+		this.origin = origin;
+		this.destination = destination;
+		this.date = date;
+		this.targetPrice = targetPrice;
+		this.currPrice = currPrice;
+	}
+
+	public void sendMail(String subject, String body) {
+
+		this.subject = subject;
+		this.body = body;
+
+	      // Setting up mail server
+	      Properties properties = new Properties();
+	      properties.put("mail.transport.protocol", "smtp");
+	      properties.put("mail.smtp.host", "smtp.gmail.com");
+	      properties.put("mail.smtp.port", "587");
+
+	      //Issue STARTTLS command
+	      properties.put("mail.smtp.starttls.enable", "true");
+
+	      //Authentication for host email
+	      properties.put("mail.smtp.auth", "true");
+
+	      Authenticator authenticator = new Authenticator() {
+	          protected PasswordAuthentication getPasswordAuthentication() {
+	              return new PasswordAuthentication("sjsuflighttracker@gmail.com", "memes1337");
+	          }
+	      };
+
+	      // creating session object to get properties
+	      Session session = Session.getDefaultInstance(properties, authenticator);
+
+	      try
+	      {
+
+	         // MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From Field: adding senders email to from field.
+	         message.setFrom(new InternetAddress(sender));
+
+	         // Set To Field: adding recipient's email to from field.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+	         // Set Subject: subject of the email
+	         message.setSubject(subject);
+
+	         // set body of the email.
+	         message.setText(body);
+
+	         // Send email.
+	         Transport.send(message);
+	         System.out.println("Mail successfully sent :)");
+	      }
+	      catch (MessagingException mex)
+	      {
+	         mex.printStackTrace();
+	      }
+	}
+
+
+	//@Overload
+	public void sendMail() {
+		body = "Flight " + origin + " to " + destination + ", on " + date + " has fallen below " + targetPrice
+				+ " and is now " + currPrice + ". Prices are subject to change quickly, so book now!";
+
+		subject = "Flight Tracker Update";
+
+	      // Setting up mail server
+	      Properties properties = new Properties();
+	      properties.put("mail.transport.protocol", "smtp");
+	      properties.put("mail.smtp.host", "smtp.gmail.com");
+	      properties.put("mail.smtp.port", "587");
+
+	      //Issue STARTTLS command
+	      properties.put("mail.smtp.starttls.enable", "true");
+
+	      //Authentication for host email
+	      properties.put("mail.smtp.auth", "true");
+
+	      Authenticator authenticator = new Authenticator() {
+	          protected PasswordAuthentication getPasswordAuthentication() {
+	              return new PasswordAuthentication("sjsuflighttracker@gmail.com", "memes1337");
+	          }
+	      };
+
+	      // creating session object to get properties
+	      Session session = Session.getDefaultInstance(properties, authenticator);
+
+	      try
+	      {
+	         // MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From Field: adding senders email to from field.
+	         message.setFrom(new InternetAddress(sender));
+
+	         // Set To Field: adding recipient's email to from field.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+	         // Set Subject: subject of the email
+	         message.setSubject(subject);
+
+	         // set body of the email.
+	         message.setText(body);
+
+	         // Send email.
+	         Transport.send(message);
+	         System.out.println("Mail successfully sent :)");
+	      }
+	      catch (MessagingException mex)
+	      {
+	         mex.printStackTrace();
+	      }
+	}
+
+
+}
---
+++ testApi/src/main/java/FlightApi.java
@@ -0,0 +1,73 @@
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class FlightApi {
+	
+
+	/**
+	 * 
+	 * @param origin of the flight
+	 * @param destination of the flight
+	 * @param email to send to
+	 * @param price of flight to check
+	 */
+	public String APICall(String origin, String destination,String date, String email, String price) {
+		try {
+//			HttpResponse<JsonNode> postCreateresponse = Unirest.post("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0")
+//					.header("X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+//					.header("X-RapidAPI-Key", "a1928a77c1msh587c896a62837fbp1e5cb2jsne4bc0c910566")
+//					.header("Content-Type", "application/x-www-form-urlencoded")
+//					.body("cabinClass=business&children=0&infants=0&country=US&currency=USD&locale=en-US&originPlace=SFO-sky&destinationPlace=LHR-sky&outboundDate=2019-12-20&adults=1")
+//					.asJson();
+
+//				List<String> location = postCreateresponse.getHeaders().get("Location");
+//				String seshKey = location.get(0).replaceAll("http://partners.api.skyscanner.net/apiservices/pricing/uk2/v1.0/", "");
+//				System.out.println(seshKey);
+				
+				
+				String country = "US";
+				String currency = "USD";
+				String locale = "en-US";
+				String originPlace = origin+ "-sky";
+				String destinationPLace = destination + "-sky";
+				String outBoundDate = date;
+				String requiredParams = String.format("/%1$s/%2$s/%3$s/%4$s/%5$s/%6$s", country, currency,locale,originPlace,destinationPLace,outBoundDate);
+				String url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0" + requiredParams;
+		
+				
+				HttpResponse<JsonNode> response = Unirest.get(url)
+						.header("X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+						.header("X-RapidAPI-Key", "a1928a77c1msh587c896a62837fbp1e5cb2jsne4bc0c910566")
+						.asJson();
+				
+				System.out.println(response.getBody().getObject());
+				
+				String test = getMin(response.getBody().getObject());
+				System.out.println(test);
+				
+				
+				return test;
+			} catch (UnirestException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return e.toString();
+			}
+	}
+	
+	public static String getMin(JSONObject obj){
+	    JSONArray quotes =  obj.getJSONArray("Quotes");
+	    JSONObject quotesItems = (JSONObject) quotes.get(0);
+	    Double minPrice = (Double) quotesItems.get("MinPrice");
+	    
+	    return Double.toString(minPrice);
+	  }
+
+	
+}
